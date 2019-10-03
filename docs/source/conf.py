a4567223d348aa272dfa513a037ea50b2fec8704
ecasellas <enric.casellas@gmail.com>
2019-10-03 13:43:32 +0200

    Adds logo to readthedocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -81,6 +81,7 @@ pygments_style = None
 #
 # html_theme = 'alabaster'
 html_theme = 'sphinx_rtd_theme'
+html_logo = '_static/logoM.svg'
 numfig = True
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
