72b60ee924c59eb0fbbf8e5d1f2b0d0f08c2622a
ecasellas <enric.casellas@gmail.com>
2023-10-04 13:04:41 +0200

    Add a function to get tif from array in geotools

--- pymica/utils/geotools.py
+++ pymica/utils/geotools.py
@@ -2,7 +2,9 @@
 """
 import math
 
+import numpy as np
 import pyproj
+from osgeo import gdal, osr
 
 
 def get_utm_epsg_from_lonlat(longitude: float, latitude: float) -> str:
@@ -17,9 +19,9 @@ def get_utm_epsg_from_lonlat(longitude: float, latitude: float) -> str:
     """
     zone = (math.floor((longitude + 180) / 6) % 60) + 1
     south = " +south " if latitude < 0 else " "
-    desc = (
-        "+proj=utm +zone={}{}+ellps=WGS84 +datum=WGS84 +units=m +no_defs"
-    ).format(zone, south)
+    desc = ("+proj=utm +zone={}{}+ellps=WGS84 +datum=WGS84 +units=m +no_defs").format(
+        zone, south
+    )
 
     utm_epsg = pyproj.CRS.from_proj4(desc).to_epsg()
 
@@ -42,3 +44,26 @@ def reproject_point(point: tuple, in_proj: int | str, out_proj: int | str) -> tu
     point_x, point_y = reproject.transform(point[0], point[1])
 
     return (point_x, point_y)
+
+
+def get_tif_from_array(
+    file_path: str, data: np.array, geotransform: np.array, projection: int
+) -> None:
+    """Save an array into a raster TIF file.
+
+    Args:
+        file_path (str): Output file path.
+        data (np.array): Array to be saved as a raster file.
+        geotransform (np.array): GDAL Geotransform.
+        projection (int): EPSG projection.
+    """
+    driver = gdal.GetDriverByName("GTiff")
+    ds_out = driver.Create(file_path, data.shape[1], data.shape[0], 1, gdal.GDT_Float32)
+    ds_out.GetRasterBand(1).WriteArray(data)
+    ds_out.GetRasterBand(1).SetNoDataValue(0)
+    ds_out.SetGeoTransform(geotransform)
+    spatialRef = osr.SpatialReference()
+    spatialRef.ImportFromEPSG(projection)
+    ds_out.SetProjection(str(spatialRef))
+
+    ds_out = None
